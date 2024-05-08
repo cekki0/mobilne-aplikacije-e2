b94f2bf29ca7c0558a19b4e35374dd087df08538
gagi155 <maslac.ra151.2020@gmail.com>
2024-05-08 22:09:51 +0200

    some crazy fixes

--- app/src/main/java/com/example/fijiapp/model/Event.java
+++ app/src/main/java/com/example/fijiapp/model/Event.java
@@ -1,6 +1,9 @@
 package com.example.fijiapp.model;
 
+import com.google.firebase.firestore.PropertyName;
+
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Event {
@@ -10,11 +13,51 @@ public class Event {
     public LocalTime EndTime;
     public EventTypes Type;
 
+    public Event() {
+        // Required empty constructor for Firestore deserialization
+    }
+
     public Event(String eventName, Date date, LocalTime startTime, LocalTime endTime, EventTypes type) {
         this.EventName = eventName;
         this.Date = date;
         this.StartTime = startTime;
         this.EndTime = endTime;
         this.Type = type;
+    }
+
+    @PropertyName("StartTime")
+    public String getStartTimeString() {
+        if (this.StartTime != null) {
+            return this.StartTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        } else {
+            return null;
+        }
+    }
+
+    @PropertyName("StartTime")
+    public void setStartTimeString(String timeString) {
+        if (timeString != null) {
+            this.StartTime = LocalTime.parse(timeString, DateTimeFormatter.ISO_LOCAL_TIME);
+        } else {
+            this.StartTime = null;
+        }
+    }
+
+    @PropertyName("EndTime")
+    public String getEndTimeString() {
+        if (this.EndTime != null) {
+            return this.EndTime.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        } else {
+            return null;
+        }
+    }
+
+    @PropertyName("EndTime")
+    public void setEndTimeString(String timeString) {
+        if (timeString != null) {
+            this.EndTime = LocalTime.parse(timeString, DateTimeFormatter.ISO_LOCAL_TIME);
+        } else {
+            this.EndTime = null;
+        }
     }
 }
